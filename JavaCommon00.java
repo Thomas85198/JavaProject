37293a7eb59f97a1962f5f19cf2ef5acc50a6617
“盧建霖” <luchienlin1994@gmail.com>
2019-11-02 14:36:25 +0800

    upload my first Java

---
+++ JavaCommon00.java
@@ -0,0 +1,10 @@
+package tw.luchienlin.javacommon;
+
+public class JavaCommon00 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Java");
+	}
+
+}
